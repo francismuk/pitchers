58d2d0240e0d1917fa439269c806d9030894c982
Francis Mukuha <mukuha58@gmail.com>
2019-06-28 10:56:15 +0300

    add flask login

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://francis:1234@localhost/newlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
