ba6210aaef5328738e64c18594cffb468f56c82b
Francis Mukuha <mukuha58@gmail.com>
2019-07-03 10:15:13 +0300

    deployment

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate,MigrateCommand
 import os
 
 app = create_app('production')
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+# SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://francis:1234@localhost/nlist' 
 
 # Create manager instance 
